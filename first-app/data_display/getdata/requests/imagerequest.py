96cdc746577ca70984d4368b28a5a5d73201d286
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-02 14:15:42 +0600

    docs:change store image file name.
    1.when get image then save this image by temp.png before was python.png.

--- first-app/data_display/getdata/requests/imagerequest.py
+++ first-app/data_display/getdata/requests/imagerequest.py
@@ -5,6 +5,6 @@ data = pip._vendor.requests.get('https://imgs.xkcd.com/comics/python.png')
 
 #image-name.image-type
 print(data)
-with open('python.png', 'wb') as f:
+with open('temp.png', 'wb') as f:
     f.write(data.content)
 # see project folder this image because use file management
